bea697afe4fa8d7eba78ec650456243afdef5477
mtiganov <mtiganov@gmcs.ru>
2015-11-05 15:32:34 +0300

    Первый коммит

---
+++ MultiplayerBalanceSample/Assets/Enemies/scripts/Common.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct MyContext
+{
+    #region  изменяемые значения 
+    public static int EnemiesExist = 0;
+    public static int EnemiesEscaped = 0;
+    public static int EnemiesDestroyed = 0;
+    #endregion
+
+    #region константы
+    //враг-куб минимальный порог спавна, сек
+    public const float CubeEnemySpawnMin = 5;
+    //враг-куб максимальный порог спавна, сек
+    public const float CubeEnemySpawnMax = 120;
+    //враг-куб минимальный порог жизни, сек
+    public const float CubeEnemyLiveMin = 60;
+    //враг-куб максимальный порог жизни, сек
+    public const float CubeEnemyLiveMax = 100;
+    //враг-куб минимальный порог смены состояния
+    public const float CubeEnemyChangeStateMin = 1;
+    //враг куб максимальный порог смены состояния
+    public const float CubeEnemyChangeStateMax = 10;
+    //дальность лазера
+    public const float MaxLaserDistance = 100;
+    #endregion
+}
---
+++ MultiplayerBalanceSample/Assets/Enemies/scripts/HudScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudScript : MonoBehaviour {
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 30), "Enemies Exist: " + MyContext.EnemiesExist);
+        GUI.Label(new Rect(10, 40, 200, 30), "Enemies Destroyed: " + MyContext.EnemiesDestroyed);
+        GUI.Label(new Rect(10, 70, 200, 30), "Enemies Escaped: " + MyContext.EnemiesEscaped);
+    }
+    void Update()
+    {
+        if (MyContext.EnemiesDestroyed == 10)
+            Application.LoadLevel("Win");
+        if (MyContext.EnemiesEscaped == 10)
+            Application.LoadLevel("GameOver");
+
+    }
+}
